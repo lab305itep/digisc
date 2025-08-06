9369a8af59f86281b196e7ae57794930d2603204
Igor Alekseev <igor@pcb305.itep.ru>
2025-08-07 01:03:27 +0300

    Working on data till run 175528

--- deadchan/calc_eff.C
+++ deadchan/calc_eff.C
@@ -7,8 +7,6 @@ void calc_eff(int NVAR, const char *outname, const char *vardir, int MCTotal = 2
 	TFile *f;
 	FILE *fEff;
 	FILE *fOut;
-	int run, var;
-	char *tok;
 	
 	memset(eff, 0, sizeof(eff));
 	for (i=0; i<NVAR; i++) {
@@ -30,4 +28,59 @@ void calc_eff(int NVAR, const char *outname, const char *vardir, int MCTotal = 2
 	}
 	for (i=0; i<NVAR; i++) fprintf(fEff, "%2d %f\n", i, eff[i]);
 	fclose(fEff);
+}
+
+void sum_fuel(int NVAR, const char *dirbase)
+{
+	int i, j;
+	char str[1024];
+	TH1D *h;
+	TH1D *hSum;
+	TFile *f[4];
+	TFile *fOut;
+	
+	for (i=0; i<NVAR; i++) {
+		sprintf(str, "%s_235U/var_%d_spfuel.root", dirbase, i);
+		f[0] = new TFile(str);
+		if (!f[0]->IsOpen()) continue;
+		sprintf(str, "%s_238U/var_%d_spfuel.root", dirbase, i);
+		f[1] = new TFile(str);
+		if (!f[1]->IsOpen()) continue;
+		sprintf(str, "%s_239Pu/var_%d_spfuel.root", dirbase, i);
+		f[2] = new TFile(str);
+		if (!f[2]->IsOpen()) continue;
+		sprintf(str, "%s_241Pu/var_%d_spfuel.root", dirbase, i);
+		f[3] = new TFile(str);
+		if (!f[3]->IsOpen()) continue;
+		hSum = (TH1D *) f[0]->Get("hMC");
+		if (!hSum) {
+			printf("hMC not found in %s\n", f[0]->GetName());
+			continue;
+		}
+		h = (TH1D *) f[1]->Get("hMC");
+		if (!h) {
+			printf("hMC not found in %s\n", f[1]->GetName());
+			continue;
+		}
+		hSum->Add(h);
+		h = (TH1D *) f[2]->Get("hMC");
+		if (!h) {
+			printf("hMC not found in %s\n", f[2]->GetName());
+			continue;
+		}
+		hSum->Add(h);
+		h = (TH1D *) f[3]->Get("hMC");
+		if (!h) {
+			printf("hMC not found in %s\n", f[3]->GetName());
+			continue;
+		}
+		hSum->Add(h);
+		sprintf(str, "%s/var_%d_spfuel.root", dirbase, i);
+		fOut = new TFile(str, "RECREATE");
+		if (!fOut->IsOpen()) continue;
+		fOut->cd();
+		hSum->Write();
+		fOut->Close();
+		for (j=0; j<4; j++) f[j]->Close();
+	}
 }
--- run_dead_mpi.sh
+++ run_dead_mpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N deadtime
 #PBS -q mpi
-#PBS -l nodes=31
+#PBS -l nodes=8
 #PBS -l walltime=100:00:00
 #PBS -o /home/clusters/rrcmpi/alekseev/igor/tmp/deadtime_calc8n2.out
 #PBS -e /home/clusters/rrcmpi/alekseev/igor/tmp/deadtime_calc8n2.err
@@ -9,6 +9,6 @@ date
 cd /home/itep/alekseev/igor
 export DEAD_PROG=deadtime
 mkdir -p /home/clusters/rrcmpi/alekseev/igor/root8n2/deadtime
-mpirun --mca btl ^tcp run_dead_mpi 151900 500 /home/clusters/rrcmpi/alekseev/igor/root8n2
+mpirun --mca btl ^tcp run_dead_mpi 167309 1030 /home/clusters/rrcmpi/alekseev/igor/root8n2
 date
 exit 0
