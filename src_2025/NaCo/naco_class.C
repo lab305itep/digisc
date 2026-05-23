53df6fc3fe63a1d09d6f6f289f162b28dc4b915b
Igor Alekseev <igor@pcb305.itep.ru>
2026-05-23 15:52:50 +0300

    Adding NaCo class

--- src_2025/NaCo/naco_class.C
+++ src_2025/NaCo/naco_class.C
@@ -1,28 +1,64 @@
 #include "../../evtbuilder.h"
-/**********************************************************
-	Process data and MC for 248Cm measurements
-248Cm runs:
-Position  Date   Runs
-center    mar17  14428-14485
-center    nov18  50578-50647
-center    jun22  127720-127772
-center    may25  175471-175528
-edge      mar17  14487-14512
-edge      nov18  50875-50947
-***********************************************************/
+/***************************************************************************************
+*			Runs with sources
+* Runs		Source	Date		Position	Nruns
+* 12198-12303	none	2017-02-03	down
+* 12306-12346	60Co	2017-02-04	center		41
+* 12348-12361	60Co	2017-02-05	edge		14
+* 12364-12373	22Na	2017-02-05	edge		10
+* 12376-12407	22Na	2017-02-05	center		32
+* 12411-12421	none	2017-02-06	down		later position was changed to up
+* 50750-50873	none	2018-11-18	down
+* 50949-50997	60Co	2018-11-20	center		49
+* 50999-51034	60Co	2018-11-21	edge		36
+* 51036-51095	22Na	2018-11-21	edge		60
+* 51099-51161	22Na	2018-11-22	center		63
+* 51167-51267	none	2018-11-23	down
+* 127280-127379	none	2022-06-19	down
+* 127389-127456	22Na	2022-06-21	center		68
+* 127462-127512	none	2022-06-22	down
+* 127513-127580	24Na	2022-06-23	center, not seen, strong pickup (?)
+* 127581-127657	none	2022-06-24	down
+* 127658-127719	24Na	2022-06-25	center, not seen
+* 127720-127772	248Cm	2022-06-26	center		53
+* 127774-127837	60Co	2022-06-27	center		64
+* 127838-127900	none	2022-06-28	down
+* 127901-127957	none	2022-06-29	up
+* 127958-128016	22Na	2022-06-30	center, UP	59
+* 128020-128119	none	2022-07-01	up
+* 174324-174343	none	2025-04-22	down		20
+* 174345-174400	60Co	2025-04-22	center		58
+* 174403-174457	Th	2025-04-23	center		55
+* 174459-174496	none	2025-04-24	down		38
+* 174720-174828	none	2025-04-30	down		109
+* 174830-175058	22Na	2025-05-01	center		229
+* 175060-175114	none	2025-05-05	down		55
+*
+***************************************************************************************
+* Center position (50, 50, 50)
+* Edge position   (50, 90, 50)
+***************************************************************************************
+* The channel for teflon tube to introduce sources is machined in left low part of 
+* strip X=12 Z=49. So strips X=11,12 Z=49 and Y=12 Z=48 could be afected by beta
+* reaching it through the capsule. We remove events with these strips hit
+***************************************************************************************/
+
+const double centerX = 50;
+const double centerY = 50;
+const double centerZ = 50;
+
 struct EnergyStruct {
 	double SiPM;
 	double PMT;
 };
 
-class CmClass {
+class NaCoClass {
 private:
 	int kEmin;	// Energy range
 	int kEmax;
 	double Emin;
 	double Emax;
-	int NMin;	// Neutron range
-	int NMax;
+	double Rcut;
 	TRandom *rndm;
 	TChain *tMC;
 	TH1D *hExp;
@@ -31,7 +67,7 @@ private:
 	TFile *fExp;
 	struct EnergyStruct *MCEnergy;
 	int NMC;
-	struct DanssCmStruct MCdata;
+	struct DanssEventStruct7 MCdata;
 	double kScaleSiPM, kScalePMT;	// energy scale
 	double kRndmSiPM, kRndmPMT;	// proportional smearing
 	double kSqrtSiPM, kSqrtPMT;	// sqrt smearing
@@ -43,19 +79,20 @@ private:
 	double calcE(double E, double kScl, double kRnd, double kSqr);
 	void Erase(const char *name);
 public:
-	CmClass(const char *expname, const char *mcname, const char *name);
-	~CmClass(void);
+	NaCoClass(const char *expname, const char *mcname, const char *name);
+	~NaCoClass(void);
 	void SetErange(int nMin, int nMax);
 	void SetErange(double rMin, double rMax);
 	inline void SetScale(double kSiPM, double kPMT) { kScaleSiPM = kSiPM; kScalePMT = kPMT; };
 	inline void SetRndm(double kSiPM, double kPMT) { kRndmSiPM = kSiPM; kRndmPMT = kPMT; };
 	inline void SetSqrt(double kSiPM, double kPMT) { kSqrtSiPM = kSiPM; kSqrtPMT = kPMT; };
+	inline void SetRcut(double val) { Rcut = val; };
 	void ScanScale(int nDiv, double sMin, double sMax, int iSet);
 	void ScanRndm(int nDiv, double sMin, double sMax, int iSet);
 	void ScanSqrt(int nDiv, double sMin, double sMax, int iSet);
 };
 
-CmClass::CmClass(const char *expname, const char *mcname, const char *name)
+NaCoClass::NaCoClass(const char *expname, const char *mcname, const char *name)
 {
 	printf("Making class for Run=%s and MC=%s\n", expname, mcname);
 	hExp = hExpSiPM = hExpPMT = NULL;
@@ -64,18 +101,17 @@ CmClass::CmClass(const char *expname, const char *mcname, const char *name)
 	Name = strdup(name);
 	MCEnergy = NULL;
 	NMC = 0;
-	NMin = 1;
-	NMax = 6;
 	kScaleSiPM = kScalePMT = 1.0;
 	kRndmSiPM = kRndmPMT = 0.08;
 	kSqrtSiPM = kSqrtPMT = 0.04;
+	Rcut = 30;	// cm
 	rndm = new TRandom2(time(NULL));
 	
 	fExp = new TFile(expname);
 	if (!fExp->IsOpen()) return;
-	hExp = (TH1D *) fExp->Get("hCm");
-	hExpSiPM = (TH1D *) fExp->Get("hCmSiPM");
-	hExpPMT = (TH1D *) fExp->Get("hCmPMT");
+	hExp = (TH1D *) fExp->Get("hExpC");
+	hExpSiPM = (TH1D *) fExp->Get("hExpSiPMC");
+	hExpPMT = (TH1D *) fExp->Get("hExpPMTC");
 	if (!hExp || !hExpSiPM || !hExpPMT) {
 		printf("Not all histograms found in %s.\n", expname);
 		return;
@@ -93,17 +129,17 @@ CmClass::CmClass(const char *expname, const char *mcname, const char *name)
 	hExpPMT->SetMarkerStyle(kFullCircle);
 	hExpPMT->SetMarkerSize(0.03);
 
-	tMC = create_chain(mcname, "DanssCm");
+	tMC = create_chain(mcname, "DanssEvent");
 	if (!tMC) {
 		printf("Can not open MC: %s\n", mcname);
 		return;
 	}
-	tMC->SetBranchAddress("Cm", &MCdata);
+	tMC->SetBranchAddress("Data", &MCdata);
 	MakeMCArray();
-	SetErange(16, 95);
+	SetErange(0.8, 2.9);
 }
 
-CmClass::~CmClass(void)
+NaCoClass::~NaCoClass(void)
 {
 	if (MCEnergy) free(MCEnergy);
 	if (fExp && fExp->IsOpen()) delete fExp;
@@ -112,20 +148,21 @@ CmClass::~CmClass(void)
 	delete rndm;
 }
 
-void CmClass::Erase(const char *name)
+void NaCoClass::Erase(const char *name)
 {
 	TObject *obj = gROOT->FindObject(name);
 	if (obj) delete obj;
 }
 
-void CmClass::MakeMCArray(void)
+void NaCoClass::MakeMCArray(void)
 {
 	int N;
-	int i, j;
+	int i;
+	double r2;
 	
 	N = tMC->GetEntries();
 	if (MCEnergy) free(MCEnergy);
-	MCEnergy = (struct EnergyStruct *) malloc(N * (NMax - NMin + 1) * sizeof(struct EnergyStruct));	// Maximum possible
+	MCEnergy = (struct EnergyStruct *) malloc(N * sizeof(struct EnergyStruct));	// Maximum possible
 	if (!MCEnergy) {
 		printf("Can not allocate memory for %d events\n", N);
 		return;
@@ -133,21 +170,30 @@ void CmClass::MakeMCArray(void)
 	NMC = 0;
 	for (i=0; i<N; i++) {
 		tMC->GetEntry(i);
-		for (j = NMin; j <= NMax && j < MCdata.N; j++) { 
-			// Cut: "N>j && gtDiff[j]/125<50 && gtDiff[j]/125>2 && PmtCleanEnergy[j]*scale > 0.7"
-			if (MCdata.gtDiff[j]/125 <= 2 || MCdata.gtDiff[j]/125 >= 50) continue;
-			if (MCdata.PmtCleanEnergy[j] <= 0.7) continue;
-			MCEnergy[NMC].SiPM = MCdata.SiPmCleanEnergy[j];
-			MCEnergy[NMC].PMT = MCdata.PmtCleanEnergy[j];
-			NMC++;
-		}
+//	TCut cxyz("NeutronX[0] >= 0 && NeutronX[1] >= 0 && NeutronX[2] >= 0");
+		if (MCdata.NeutronX[0] < 0) continue;
+		if (MCdata.NeutronX[1] < 0) continue;
+		if (MCdata.NeutronX[2] < 0) continue;
+//	TCut cVeto("VetoCleanHits < 2 && VetoCleanEnergy < 4");
+		if (MCdata.VetoCleanHits >= 2 || MCdata.VetoCleanEnergy >= 4) continue;
+//	TCut cn("SiPmCleanHits > 2");
+		if (MCdata.SiPmCleanHits <= 2) continue;
+		if (MCdata.PmtCleanHits < 1) continue;
+//	Cut over r2
+		r2  = (MCdata.NeutronX[0] + 2 - centerX) * (MCdata.NeutronX[0] + 2 - centerX);
+		r2 += (MCdata.NeutronX[1] + 2 - centerY) * (MCdata.NeutronX[1] + 2 - centerY);
+		r2 += (MCdata.NeutronX[2] + 0.5 - centerZ) * (MCdata.NeutronX[2] + 0.5 - centerZ);
+		if (r2 > Rcut*Rcut) continue;		// 30 cm
+		MCEnergy[NMC].SiPM = MCdata.SiPmCleanEnergy;
+		MCEnergy[NMC].PMT = MCdata.PmtCleanEnergy;
+		NMC++;
 	}
-	printf("MC: %d neutrons found in %d fissions\n", NMC, N);
+	printf("MC: %d events from %d triggers\n", NMC, N);
 }
 
 //	Calculate chi^2 difference of two histograms
 //  hcm, hMc - 1-dim histograms
-double CmClass::chi2Diff(TH1 *hcm, TH1 *hMc)
+double NaCoClass::chi2Diff(TH1 *hcm, TH1 *hMc)
 {
 	int i;
 	double chi2 = 0;
@@ -162,7 +208,7 @@ double CmClass::chi2Diff(TH1 *hcm, TH1 *hMc)
 //	Create a chain and adds files to it
 //  fname   - file name(s)
 //  chname  - the chainname
-TChain *CmClass::create_chain(const char *fname, const char *chname)
+TChain *NaCoClass::create_chain(const char *fname, const char *chname)
 {
 	char *tok;
 	const char *DELIM = " \t,;";
@@ -189,7 +235,7 @@ TChain *CmClass::create_chain(const char *fname, const char *chname)
 //	kScl - scale
 //	kRnd - proportional smearing
 //	kSqr - stokhastic smearing
-double CmClass::calcE(double E, double kScl, double kRnd, double kSqr)
+double NaCoClass::calcE(double E, double kScl, double kRnd, double kSqr)
 {
 	double X = kScl * E;
 	double S = sqrt(kRnd * kRnd * X * X + kSqr * kSqr * X);
@@ -198,7 +244,7 @@ double CmClass::calcE(double E, double kScl, double kRnd, double kSqr)
 
 //	Set range for histogram compare
 //	nMin, nMax - bin numbers
-void CmClass::SetErange(int nMin, int nMax)
+void NaCoClass::SetErange(int nMin, int nMax)
 {
 	kEmin = nMin;
 	kEmax = nMax;
@@ -212,7 +258,7 @@ void CmClass::SetErange(int nMin, int nMax)
 
 //	Set range for histogram compare
 //	rMin, rMax - minimum and maximum energy
-void CmClass::SetErange(double rMin, double rMax)
+void NaCoClass::SetErange(double rMin, double rMax)
 {
 	if(!hExp) {
 		printf("ERROR: No histogramm!\n");
@@ -224,7 +270,7 @@ void CmClass::SetErange(double rMin, double rMax)
 	Emax = hExp->GetXaxis()->GetBinUpEdge(kEmax);
 }
 
-void CmClass::ScanScale(int nDiv, double sMin, double sMax, int iSet = 0)
+void NaCoClass::ScanScale(int nDiv, double sMin, double sMax, int iSet = 0)
 {
 	double xmin;
 	double xminSiPM, xminPMT;
@@ -241,18 +287,18 @@ void CmClass::ScanScale(int nDiv, double sMin, double sMax, int iSet = 0)
 	char str[256];
 	
 	// Erase old
-	Erase("hScan248Cm");
-	Erase("hScan248CmSiPM");
-	Erase("hScan248CmPMT");
+	Erase("hScan");
+	Erase("hScanSiPM");
+	Erase("hScanPMT");
 	Erase("hTmp");
 	Erase("hTmpSiPM");
 	Erase("hTmpPMT");
 	Erase("fpol2");
 	// Book histogramms
 	ds = 0.5 * (sMax - sMin) / (nDiv - 1);
-	TH1D *hScan = new TH1D("hScan248Cm", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM+PMT;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanSiPM = new TH1D("hScan248CmSiPM", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanPMT = new TH1D("hScan248CmPMT", "#chi^{2} difference between MC and data, ^{248}Cm, PMT;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScan = new TH1D("hScan", "#chi^{2} difference between MC and data, SiPM+PMT;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanSiPM = new TH1D("hScanSiPM", "#chi^{2} difference between MC and data, SiPM;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanPMT = new TH1D("hScanPMT", "#chi^{2} difference between MC and data, PMT;Scale;#chi^{2}", nDiv, sMin - ds, sMax + ds);
 	nBins = hExp->GetXaxis()->GetNbins();
 	rLow = hExp->GetXaxis()->GetBinLowEdge(1);
 	rUp = hExp->GetXaxis()->GetBinUpEdge(nBins);
@@ -386,7 +432,7 @@ void CmClass::ScanScale(int nDiv, double sMin, double sMax, int iSet = 0)
 	if (iSet) SetScale(xminSiPM, xminPMT);
 }
 
-void CmClass::ScanRndm(int nDiv, double sMin, double sMax, int iSet = 0)
+void NaCoClass::ScanRndm(int nDiv, double sMin, double sMax, int iSet = 0)
 {
 	double xmin;
 	double xminSiPM, xminPMT;
@@ -403,18 +449,18 @@ void CmClass::ScanRndm(int nDiv, double sMin, double sMax, int iSet = 0)
 	char str[256];
 	
 	// Erase old
-	Erase("hScan248Cm");
-	Erase("hScan248CmSiPM");
-	Erase("hScan248CmPMT");
+	Erase("hScan");
+	Erase("hScanSiPM");
+	Erase("hScanPMT");
 	Erase("hTmp");
 	Erase("hTmpSiPM");
 	Erase("hTmpPMT");
 	Erase("fpol2");
 	// Book histogramms
 	ds = 0.5 * (sMax - sMin) / (nDiv - 1);
-	TH1D *hScan = new TH1D("hScan248Cm", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM+PMT;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanSiPM = new TH1D("hScan248CmSiPM", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanPMT = new TH1D("hScan248CmPMT", "#chi^{2} difference between MC and data, ^{248}Cm, PMT;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScan = new TH1D("hScan", "#chi^{2} difference between MC and data, SiPM+PMT;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanSiPM = new TH1D("hScanSiPM", "#chi^{2} difference between MC and data, SiPM;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanPMT = new TH1D("hScanPMT", "#chi^{2} difference between MC and data, PMT;Smearing;#chi^{2}", nDiv, sMin - ds, sMax + ds);
 	nBins = hExp->GetXaxis()->GetNbins();
 	rLow = hExp->GetXaxis()->GetBinLowEdge(1);
 	rUp = hExp->GetXaxis()->GetBinUpEdge(nBins);
@@ -548,7 +594,7 @@ void CmClass::ScanRndm(int nDiv, double sMin, double sMax, int iSet = 0)
 	if (iSet) SetRndm(xminSiPM, xminPMT);
 }
 
-void CmClass::ScanSqrt(int nDiv, double sMin, double sMax, int iSet = 0)
+void NaCoClass::ScanSqrt(int nDiv, double sMin, double sMax, int iSet = 0)
 {
 	double xmin;
 	double xminSiPM, xminPMT;
@@ -565,18 +611,18 @@ void CmClass::ScanSqrt(int nDiv, double sMin, double sMax, int iSet = 0)
 	char str[256];
 	
 	// Erase old
-	Erase("hScan248Cm");
-	Erase("hScan248CmSiPM");
-	Erase("hScan248CmPMT");
+	Erase("hScan");
+	Erase("hScanSiPM");
+	Erase("hScanPMT");
 	Erase("hTmp");
 	Erase("hTmpSiPM");
 	Erase("hTmpPMT");
 	Erase("fpol2");
 	// Book histogramms
 	ds = 0.5 * (sMax - sMin) / (nDiv - 1);
-	TH1D *hScan = new TH1D("hScan248Cm", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM+PMT;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanSiPM = new TH1D("hScan248CmSiPM", "#chi^{2} difference between MC and data, ^{248}Cm, SiPM;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
-	TH1D *hScanPMT = new TH1D("hScan248CmPMT", "#chi^{2} difference between MC and data, ^{248}Cm, PMT;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScan = new TH1D("hScan", "#chi^{2} difference between MC and data, SiPM+PMT;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanSiPM = new TH1D("hScanSiPM", "#chi^{2} difference between MC and data, SiPM;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
+	TH1D *hScanPMT = new TH1D("hScanPMT", "#chi^{2} difference between MC and data, PMT;Stochastic;#chi^{2}", nDiv, sMin - ds, sMax + ds);
 	nBins = hExp->GetXaxis()->GetNbins();
 	rLow = hExp->GetXaxis()->GetBinLowEdge(1);
 	rUp = hExp->GetXaxis()->GetBinUpEdge(nBins);
@@ -711,16 +757,19 @@ void CmClass::ScanSqrt(int nDiv, double sMin, double sMax, int iSet = 0)
 }
 
 //============================================================//
-CmClass *makeCm(int iRun, int iMC)
+NaCoClass *makeNaCo(int iRun, int iMC)
 {
 	char str[256];
+	char mclong[2048];
+	const char *mcdir = "/home/clusters/rrcmpi/alekseev/igor/root8n7/MC/Fuso/22Na";
+	const char *mcfname = "mc_22Na_indLY_transcode_rawProc_pedSim_Center1.root";
 	const char *MC[] = {
-		"cm_MC_8.7_Center_Fuso.root",
-		"cm_MC_8.7_Center_Fuso_Birks_0_005.root",
-		"cm_MC_8.7_Center_Fuso_Cher_coeff_0_05.root",
-		"cm_MC_8.7_Center_Fuso_paint_0_2.root",
-		"cm_MC_8.7_Center_Fuso_paint_0_3.root",
-		"cm_MC_8.7_Center_Fuso_PMT_strip_map.root"
+		"Full_decay_center_Fuso",
+		"Full_decay_center_Fuso_Birks_0_005",
+		"Full_decay_center_Fuso_Cher_coeff_0_05",
+		"Full_decay_center_Fuso_paint_0_2",
+		"Full_decay_center_Fuso_paint_0_3",
+		"Full_decay_center_Fuso_PMT_strip_map"
 	};
 	const char *MCshort[] = {
 		"Fuso",
@@ -731,24 +780,27 @@ CmClass *makeCm(int iRun, int iMC)
 		"Fuso_PMT_strip_map"
 	};
 	const char *Run[] = {
-		"cm_14428_14485_8.7.hist.root",
-		"cm_50578_50647_8.7.hist.root",
-		"cm_127720_127772_8.7.hist.root",
-		"cm_175471_175528_8.7.hist.root"
+		"22Na_feb17_center_root8n7_R30.0.root",
+		"22Na_nov18_center_root8n7_R30.0.root",
+		"22Na_jun22_center_root8n7_R30.0.root",
+		"60Co_feb17_center_root8n7_R30.0.root",
+		"60Co_nov18_center_root8n7_R30.0.root",
+		"60Co_jun22_center_root8n7_R30.0.root",
+		"60Co_apr25_center_root8n7_R30.0.root"
 	};
 	const char *RunShort[] = {
-		"mar17", "nov18", "jun22", "may25"
+		"mar17", "nov18", "jun22", "apr25"
 	};
+	sprintf(mclong, "%s/%s/%s", mcdir, MC[iMC], mcfname);
 	sprintf(str, "%s %s", RunShort[iRun], MCshort[iMC]);
-	CmClass *cm = new CmClass(Run[iRun], MC[iMC], str);
-	return cm;
+	NaCoClass *src = new NaCoClass(Run[iRun], mclong, str);
+	return src;
 }
 
-
-void cm_class(const char *pdfname)
+void naco_class(const char *pdfname)
 {
 	int i, j;
-	CmClass *cm;
+	NaCoClass *src;
 	
 	gStyle->SetOptStat(0);
 	TCanvas *cv = (TCanvas *) gROOT->FindObject("CV");
@@ -756,23 +808,23 @@ void cm_class(const char *pdfname)
 	TString pdf(pdfname);
 	cv->SaveAs((pdf+"[").Data());
 	for (i=0; i<6; i++) for (j=0; j<4; j++) {
-		cm = makeCm(j, i);
-		if (!cm) {
+		src = makeNaCo(j, i);
+		if (!src) {
 			printf("Fatal error !\n");
 			goto fin;
 		}
-		cm->SetErange(5.5, 9.0);
-		cm->SetSqrt(0.04, 0.04);
+		src->SetErange(5.5, 9.0);
+		src->SetSqrt(0.04, 0.04);
 		if (i<5) {
-			cm->SetRndm(0.08, 0.07);
+			src->SetRndm(0.08, 0.07);
 		} else {
-			cm->SetRndm(0.08, 0.05);
+			src->SetRndm(0.08, 0.05);
 		}
-		cm->ScanScale(20, 0.87, 1.07, 1);
+		src->ScanScale(20, 0.87, 1.07, 1);
 		cv->SaveAs(pdf.Data());
-		cm->ScanRndm(15, 0.04, 0.115);
+		src->ScanRndm(15, 0.04, 0.115);
 		cv->SaveAs(pdf.Data());
-		delete cm;
+		delete src;
 	}
 fin:
 	cv->SaveAs((pdf+"]").Data());
